d5c88784d1215312950d9bb9423b41c0f77c50f8
Gavin Li <gavinli@thegavinli.com>
2015-02-17 02:31:18 -0800

    add basic polling demo

--- demo.c
+++ demo.c
@@ -18,6 +18,22 @@ void startthread_demo() {
 	coroo_thread_exit();
 }
 
+void basicpoll_thread(void *aux) {
+	while (true) {
+		printf("start poll\n");
+		coroo_poll_simple(0, POLLIN);
+		printf("poll complete\n");
+		char buf[512];
+		ssize_t cnt = read(0, buf, sizeof(buf));
+		write(1, buf, cnt);
+	}
+}
+
+void basicpoll_demo() {
+	coroo_thread_start(65536, basicpoll_thread, NULL);
+	coroo_thread_exit();
+}
+
 int main(int argc, char *argv[]) {
 	coroo_thread_init();
 	if (argc < 2) {
@@ -27,6 +43,8 @@ int main(int argc, char *argv[]) {
 	const char *demo_name = argv[1];
 	if (strcmp(demo_name, "startthread") == 0) {
 		startthread_demo();
+	} else if (strcmp(demo_name, "basicpoll") == 0) {
+		basicpoll_demo();
 	} else {
 		fprintf(stderr, "unknown demo name!\n");
 		return EXIT_FAILURE;
