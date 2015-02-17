5504f1f4f4df438d12dd7d06cb57d925be718aea
Gavin Li <gavinli@thegavinli.com>
2015-02-17 00:15:42 -0800

    initial commit

---
+++ coroo.h
@@ -0,0 +1,7 @@
+typedef struct CorooThread CorooThread;
+typedef void (*CorooThreadFunction)(void *);
+
+void coroo_thread_init();
+CorooThread *coroo_thread_start(size_t stack_size,
+		CorooThreadFunction thread_function,
+		void *thread_argument);
---
+++ coroo_core.c
@@ -0,0 +1,145 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include "coroo.h"
+
+typedef enum {
+	STACK_DIRECTION_UNKNOWN,
+	STACK_DIRECTION_UP,
+	STACK_DIRECTION_DOWN,
+} StackDirection;
+
+struct CorooThread {
+	void *stack_base;
+	size_t stack_size; // including guard page
+	CorooThreadFunction thread_function;
+	void *thread_argument;
+};
+
+StackDirection stack_direction = STACK_DIRECTION_UNKNOWN;
+CorooThread main_thread;
+
+static void maybe_clobber_pointer(void **ptr) {
+	if (stack_direction == STACK_DIRECTION_UNKNOWN) {
+		uintptr_t value = (uintptr_t)(*ptr);
+		union {
+			char buf;
+			struct {
+				uintptr_t value;
+				char end;
+			};
+		} unzer;
+		unzer.value = value;
+		unzer.end = '\0';
+		strtok(&unzer.buf, "");
+		*ptr = (void *)(value | unzer.value);
+	}
+}
+
+static void determine_stack_direction_actual(void *prev) {
+	if (stack_direction != STACK_DIRECTION_UNKNOWN)
+		return;
+	if ((void *)&prev < prev) {
+		stack_direction = STACK_DIRECTION_DOWN;
+	} else if ((void *)&prev > prev) {
+		stack_direction = STACK_DIRECTION_UP;
+	} else {
+		fputs("critical error: failed to determine stack direction!\n", stderr);
+		abort();
+	}
+}
+
+static void determine_stack_direction(void *prev) {
+	void (*func)(void *) = determine_stack_direction_actual;
+	maybe_clobber_pointer((void **)&func);
+	func(prev);
+}
+
+static void thread_invoke_function_actual(CorooThread *thread, char *filler) {
+	thread->thread_function(thread->thread_argument);
+	// must not return
+	printf("thread function returned\n");
+	abort();
+}
+
+static void thread_invoke_function(CorooThread *thread, char *filler) {
+	void (*func)(CorooThread *, char *) = thread_invoke_function_actual;
+	maybe_clobber_pointer((void **)&func);
+	func(thread, filler);
+}
+
+static void thread_start_helper_actual(CorooThread *thread, size_t jump) {
+	char filler[jump];
+	thread_invoke_function(thread, filler);
+}
+
+static void thread_start_helper(CorooThread *thread, size_t jump) {
+	void (*func)(CorooThread *, size_t) = thread_start_helper_actual;
+	maybe_clobber_pointer((void **)&func);
+	func(thread, jump);
+}
+
+static size_t get_page_size() {
+	static size_t cached = 0;
+	if (cached == 0) {
+		cached = sysconf(_SC_PAGESIZE);
+		if (cached & (cached - 1)) {
+			fputs("critical error: page size not power of two!\n", stderr);
+			abort();
+		}
+	}
+	return cached;
+}
+
+void coroo_thread_init() {
+	// only run once
+	if (stack_direction != STACK_DIRECTION_UNKNOWN)
+		return;
+	// determine stack direction
+	int dummy;
+	determine_stack_direction(&dummy);
+	// initialize main thread
+	memset(&main_thread, 0, sizeof(main_thread));
+}
+
+CorooThread *coroo_thread_start(size_t stack_size,
+		CorooThreadFunction thread_function,
+		void *thread_argument) {
+	// round up the stack size
+	size_t page_size = get_page_size();
+	size_t page_mask = ~(page_size - 1);
+	stack_size = (stack_size + page_size - 1) & page_mask;
+	stack_size += page_size * 2; // guard page and margin page
+	// try to map in stack pages
+	void *stack_base = mmap(NULL, stack_size, PROT_READ | PROT_WRITE,
+			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (!stack_base)
+		fputs("warning: failed to map memory for stack\n", stderr);
+	// make guard page
+	void *guard_start = stack_direction == STACK_DIRECTION_DOWN ?
+		stack_base :
+		(void *)((uintptr_t)stack_base + stack_size - page_size);
+	if (mprotect(guard_start, page_size, PROT_NONE) != 0)
+		fputs("warning: failed to set guard page\n", stderr);
+	// initialize the thread
+	CorooThread *thread = malloc(sizeof(CorooThread));
+	thread->stack_base = stack_base;
+	thread->stack_size = stack_size;
+	thread->thread_function = thread_function;
+	thread->thread_argument = thread_argument;
+	// run the thread
+	size_t jump;
+	if (stack_direction == STACK_DIRECTION_DOWN)
+		jump = (uintptr_t)&thread - ((uintptr_t)stack_base + stack_size - page_size);
+	else
+		jump = ((uintptr_t)stack_base + page_size) - (uintptr_t)&jump;
+	thread_start_helper(thread, jump);
+	// return the thread
+	return thread;
+}
